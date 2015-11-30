69934f0b08a4057f18bddb5825f75215102406ae
Carlos Barajas <12barajasc@gmail.com>
2015-11-30 01:02:03 -0500

    Getting the game init setup

--- Adventure.s
+++ Adventure.s
@@ -1,6 +1,10 @@
 .data
+array: .space 2048 # 8 * 8 * 8 3-Dimensional Array (* 4 Bytes)
 backward: .asciiz "s"
-buffer: .space 6
+buffer: .space 6 # 4 input chars, a \n and the null terminator
+dimx: .word 8
+dimy: .word 8
+dimz: .word 8
 down: .asciiz "f"
 eat: .asciiz "e"
 forward: .asciiz "w"
@@ -9,17 +13,31 @@ left: .asciiz "a"
 nline: .asciiz "\n"
 prompt: .asciiz "->> "
 right: .asciiz "d"
+setup_1: .asciiz "Hello! Welcome to AssemblyAdventure!\nType h for [h]elp. The interpreter process four character commands at a time.\nAnymore than that will be ignored or worse, crunked up.\nFind the diamond and goodluck!"
+total: .word 0 # Total Moves
 up: .asciiz "r"
 quit: .asciiz "q"
+x: .word 0
+y: .word 0
+z: .word 0
 
 .text
+
+init:
+    # Print Setup Text
+    la $a0, setup_1
+    li $v0, 4
+    syscall
+
+    # Setup Game Array
 
 prompt:
     la $a0, prompt # Gather String
     li $v0, 4
     syscall
     la $a0, buffer
-    li $v0, # num for read string
+    li $a1, 5
+    li $v0, 8 # num for read string
     add $s0, $a0, $zero # Move address
     jal analyze
     li $a1, 0
@@ -27,8 +45,8 @@ prompt:
     # Reset buffer space
     la $s0, buffer
     sw $zero, $s0
-    sb $zero, $s0(1)
-    sb $zer0, $s0(2) 
+    sb $zero, $s0(4)
+    sb $zero, $s0(5) 
     b prompt
 
 exit:
@@ -91,10 +109,25 @@ exit:
     li $v0, 10
     syscall
 
+# Proces Movements
 back:
+    # Check Array Dimensions
+    # Branch
+    # Or Move
+    # Decrement HP
+    # Store $ra
+    # JAL to Win Check
+    # JAL to death check
+    # Jump prompt
 dwn:
 fwd:
 lft:
 rght:
 rise:
 eet:
+    # Check Sammich Byte
+    # Decrement Sammich Byte
+    # Increment Hp
+
+win:
+death:
